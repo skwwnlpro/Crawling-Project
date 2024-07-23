97ea8e70533368cd9739966f7572a184c533078a
jm-jung <jmsk7554@gmail.com>
2024-07-23 23:47:09 +0900

    리스트 가져오기

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -11,21 +14,21 @@ import re
 from datetime import datetime
 
 # YAML 파일 경로
-config_file = "Crawling-Project/db_config.yml"
-
-# YAML 파일 읽기
-with open(config_file, "r") as file:
-    config = yaml.safe_load(file)
+# config_file = "Crawling-Project/db_config.yml"
+#
+# # YAML 파일 읽기
+# with open(config_file, "r") as file:
+#     config = yaml.safe_load(file)
 
 # YAML 파일에서 읽은 설정 사용
-conn = pymysql.connect(
-    host=config["database"]["host"],
-    user=config["database"]["user"],
-    password=config["database"]["password"],
-    db=config["database"]["db"],
-    charset=config["database"]["charset"],
-    cursorclass=getattr(pymysql.cursors, config["database"]["cursorclass"]),
-)
+# conn = pymysql.connect(
+#     host=config["database"]["host"],
+#     user=config["database"]["user"],
+#     password=config["database"]["password"],
+#     db=config["database"]["db"],
+#     charset=config["database"]["charset"],
+#     cursorclass=getattr(pymysql.cursors, config["database"]["cursorclass"]),
+# )
 
 # 드라이버 절대 경로
 # chrome_driver_path = "C:\\Users\\Pro\\.wdm\\drivers\\chromedriver\\win64\\126.0.6478.182\\chromedriver-win32"
@@ -54,110 +57,31 @@ browser = webdriver.Chrome()
 # dates = browser.find_elements(By.CLASS_NAME, "gd_name")  # elements: 리스트
 
 # 가게 리스트 (일부만 표시)
-stores = [
-    "명동교자",
-    "옛날민속집 본점",
-    "만족 오항족발",
-    "쟈니덤플링",
-    "미즈컨테이너",
-    "마코토",
-    "을밀대",
-    "혜화돌쇠아저씨",
-    "우래옥",
-    "바토스",
-    "오율",
-    "마리쿡",
-    "청솔나무",
-    "지하손만두",
-    "초마",
-    "삼청동수제비",
-    "토속촌 삼계탕",
-    "대원갈비",
-    "청담돈가",
-    "윤씨밀방",
-    "바바인디아",
-    "육회자매집",
-    "청진옥",
-    "하카다분코",
-    "모모코",
-    "툭툭누들타이",
-    "귀족족발",
-    "테이스팅룸 이태원점",
-    "이문설농탕",
-    "리틀사이공",
-    "활화산 조개구이 칼국수",
-    "홍스쭈꾸미",
-    "어부와백정 영등포 본점",
-    "아이해브어드림",
-    "필동면옥",
-    "대장장이화덕피자",
-    "부자피자",
-    "미진",
-    "단",
-    "서부면옥",
-    "고상",
-    "역전회관",
-    "마론키친앤바",
-    "더함",
-    "새벽집",
-    "떼아떼베네",
-    "소프트리",
-    "스테파니카페 2호점",
-    "알리고떼",
-    "연남 서서갈비",
-    "대게나라 방이점",
-    "올리아 키친 앤 그로서리",
-    "서울서 둘째로 잘하는 집",
-    "노블카페 강남점",
-    "황소고집",
-    "참설농탕 송파본점",
-    "우노",
-    "순희네빈대떡",
-    "을지면옥",
-    "돈코보쌈",
-    "애플하우스",
-    "봉우화로",
-    "메리고라운드 신천점",
-    "패션 5",
-    "버터핑거팬케이크",
-    "우대가",
-    "성수족발",
-    "마도니셰프 명동점",
-    "노블카페 가로수길점",
-    "먹쉬돈나 삼청동점",
-    "부처스컷 청담",
-    "군산오징어",
-    "진주집",
-    "웃사브",
-    "미미네",
-    "뿔레치킨 홍대본점",
-    "남포면옥",
-    "밀탑",
-    "부첼라",
-    "스시효 청담본점",
-    "동빙고",
-    "서린낙지",
-    "오자오동 함흥냉면",
-    "우성갈비",
-    "평래옥",
-    "피자힐",
-    "호수삼계탕",
-    "호우양꼬치",
-    "마포 본점최대포",
-    "비스떼까",
-    "프로간장게장 본점",
-    "피자리움",
-    "마마스",
-    "맛있는 교토 1호점",
-    "영동왕족발",
-    "커피스튜디오",
-    "한일관 압구정본점",
-    "계열사",
-    "더 가든 키친",
-    "더 스테이크 하우스",
-]
 
-url = f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={stores[0]}"
+url = 'https://product.kyobobook.co.kr/detail/S000001865118'
+browser.get(url)
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "book_contents_item"))
+    )
+    content = element.get_attribute('innerHTML')
+
+    # <br> 태그를 기준으로 분할
+    items = content.split('<br>')
+
+    # 숫자와 공백을 제거하고 리스트에 추가
+    restaurant_list = [re.sub(r'^\d+\s*', '', item.strip()) for item in items if item.strip()]
+
+    print("Restaurant List:")
+    for restaurant in restaurant_list:
+        print(restaurant)
+
+    print(f"\nTotal restaurants: {len(restaurant_list)}")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+url = f"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query={restaurant_list[0]}"
 browser.get(url)
 a = browser.find_element(By.CLASS_NAME, "iBUwB").get_attribute("href")
 browser.get(a)
